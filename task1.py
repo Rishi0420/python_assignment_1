e4a721b16c8613f5e22d55786c167e18bd6c10c9
Rishi0420 <rushikeshsphatak04@gmail.com>
2025-06-01 07:35:42 +0530

    Add files via upload
    Added Task 1 and Task 2 Python scripts

---
+++ task1.py
@@ -0,0 +1,9 @@
+# Task 1: Perform Basic Mathematical Operations
+a = int(input(" Enter number: "))
+b = int(input(" Enter number: "))
+
+print( "addition is:", a+b)
+print( "subtraction is:", a-b)
+print( "multiplication is:", a*b)
+print( "division is:", float(a/b))
+
---
+++ task2.py
@@ -0,0 +1,5 @@
+# Task 2: Create a Personalized Greeting
+
+first_name = input("Enter First Name: ")
+last_name = input("Enter Last Name: ")
+print("hello, ", first_name, last_name,"! Welcome to the Python program.")
